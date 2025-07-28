62c76edce5995cfdc079cfcd31be807afdd6368d
atopy8 <Antoninpy@outlook.fr>
2025-07-28 21:17:48 +0200

    feat : start of the time logic for the refreshment rate of the game

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,10 @@
 #include <SFML/Graphics.hpp>
+// clock
+#include <SFML/System/Export.hpp>
+#include <chrono>
+#include <ratio>
+#include <type_traits>
+
 #include "../include/Game.hpp"
 
 const unsigned int widthWindow  = 1920u;
@@ -8,6 +14,9 @@ const unsigned int nbLin        = 20u;
 const unsigned int nbCol        = 80u;
 
 const unsigned int speed        = 10u;
+const unsigned int nbFood       = 1u;
+
+const unsigned int updateSpeed  = 50u;
 
 
 int main()
@@ -15,8 +24,9 @@ int main()
     auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "SnakeCPP");
     window.setFramerateLimit(144);
 
-    Game* game   = new Game(speed);
-    
+    Game* game   = new Game(speed, nbFood);
+    time_t lastTime;
+    time(&lastTime);
 
     while (window.isOpen())
     {
@@ -37,7 +47,19 @@ int main()
             }
         }
 
-        window.clear();
-        window.display();
+        
+        // visual update
+        if (difftime(time(nullptr), lastTime) > updateSpeed) {
+            
+            window.clear();
+            
+            
+            time(&lastTime);
+            game->draw();
+
+
+            window.display();
+        }
+        
     }
 }
