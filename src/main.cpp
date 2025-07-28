751d29cdd82189fe9d76a1006ac903c3e97e71b4
atopy8 <Antoninpy@outlook.fr>
2025-07-28 10:29:32 +0200

    feat : add constant for easy modification of game parameters

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,39 @@
 #include <SFML/Graphics.hpp>
+#include "../include/Game.hpp"
+
+const unsigned int widthWindow  = 1920u;
+const unsigned int heightWindow = 1080u;
+
+const unsigned int nbLin        = 20u;
+const unsigned int nbCol        = 80u;
+
+const unsigned int speed        = 10u;
+
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
+    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "SnakeCPP");
     window.setFramerateLimit(144);
+
+    Game* game   = new Game(speed);
+    
 
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
         {
-            if (event->is<sf::Event::Closed>())
-            {
+            // Closing the window
+            if (event->is<sf::Event::Closed>()){
                 window.close();
+            }// Key pressed
+            else if (const sf::Event::KeyPressed* keyPressed = event->getIf<sf::Event::KeyPressed>()){
+
+                if (keyPressed->scancode == sf::Keyboard::Scancode::Escape){
+                    window.close();
+                }
+                else {
+                    game->handleInput(keyPressed->scancode);
+                }
             }
         }
 
