c735a90fe67bc4126ca1c009a6339deeda6713ec
atopy8 <Antoninpy@outlook.fr>
2025-07-28 21:13:22 +0200

    feat : adding new logic to the food class

--- include/Food.hpp
+++ include/Food.hpp
@@ -1,9 +1,20 @@
 #pragma once
 
+#include "Position.hpp"
+
 class Food
 {
 private:
+    unsigned int number;
+    Position** positionList;
 
 public:
+    Food(unsigned int number);
+    ~Food();
 
+
+    bool eatIfFood(unsigned int x, unsigned int y);
+    bool isFoodHere(unsigned int x, unsigned int y);
+    
+    void draw();
 };
--- src/Food.cpp
+++ src/Food.cpp
@@ -1,1 +1,31 @@
+#include <stdlib.h>
 #include "../include/Food.hpp"
+
+Food::Food(unsigned int number)
+    : number(number) {
+        positionList = new Position*[number];
+    }
+
+bool Food::eatIfFood(unsigned int x, unsigned int y){
+    for (int i = 0; i<number; i++){
+        if (positionList[i]->x == x && positionList[i]->y == y){
+            positionList[i]->x = rand() % 20;
+            positionList[i]->y = rand() % 80;
+            while (isFoodHere(positionList[i]->x, positionList[i]->y)){
+                positionList[i]->x = rand() % 20;
+                positionList[i]->y = rand() % 80;
+            }
+            return true;
+        }
+    }
+    return false;
+}
+
+bool Food::isFoodHere(unsigned int x, unsigned int y) {
+    for (int i = 0; i<number; i++){
+        if (positionList[i]->x == x && positionList[i]->y == y){
+            return true;
+        }
+    }
+    return false;
+}
