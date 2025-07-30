ebfb19181683b2c3bf1b518e66be882fa20795fd
atopy8 <Antoninpy@outlook.fr>
2025-07-30 20:29:19 +0200

    feat: added gameparameters in case of futur customization

---
+++ include/GameParameters.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+class GameParameters{
+    private:
+    public:
+        unsigned int widthWindow  = 1920u;
+        unsigned int heightWindow = 1080u;
+
+        unsigned int nbLin        = 22u;
+        unsigned int nbCol        = 40u;
+
+        unsigned int speed        = 10u;
+        unsigned int nbFood       = 10u;
+
+        unsigned int updateSpeed  = 100000u;
+
+};
